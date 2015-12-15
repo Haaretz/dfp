1e6780f1c7d86a2f236c0ff8e2ad7e40f8de633c
KFlash <kflash123@gmail.com>
2015-12-16 07:44:43 +0800

    version bump

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -59,45 +59,13 @@ module.exports = function (config) {
       'mocha', 'coverage'
     ],
 
-    coverageReporter: {
-            // specify a common output directory 
-            dir: 'build/reports/coverage',
-            reporters: [
-                // reporters not supporting the `file` property 
-                {
-                    type: 'text',
-                    subdir: 'report-text',
-					dir: '../coverage'
-                }, {
-                    type: 'lcov',
-                    subdir: 'report-lcov',
-					dir: '../coverage'
-                },
-                // reporters supporting the `file` property, use `subdir` to directly 
-                // output them in the `dir` directory 
-                {
-                    type: 'cobertura',
-                    subdir: '.',
-                    file: 'cobertura.txt'
-                }, {
-                    type: 'lcovonly',
-                    subdir: '.',
-                    file: 'report-lcovonly.txt'
-                }, {
-                    type: 'teamcity',
-                    subdir: '.',
-                    file: 'teamcity.txt'
-                }, {
-                    type: 'text',
-                    subdir: '.',
-                    file: 'text.txt'
-                }, {
-                    type: 'text-summary',
-                    subdir: '.',
-                    file: 'text-summary.txt'
-                },
-            ]
-        },
+   coverageReporter: {
+      reporters: [{
+        type: 'lcov', dir: '../coverage'
+      }, {
+        type: 'text-summary', dir: '../coverage'
+      }]
+    },
 
       browsers: ['Chrome'],
         // custom launchers
