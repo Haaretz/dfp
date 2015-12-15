a85baee46264731cc945d7d2c710d2be6349725a
KFlash <kflash123@gmail.com>
2015-12-15 12:07:41 +0800

    version bump

---
+++ dist/boily.js
@@ -0,0 +1,16 @@
+/*!
+ * boily v0.0.5
+ * (c) 2015 KFlash
+ * Released under the MIT License.
+ */
+(function (global, factory) {
+    typeof exports === 'object' && typeof module !== 'undefined' ? factory() :
+    typeof define === 'function' && define.amd ? define(factory) :
+    factory();
+}(this, function () { 'use strict';
+
+    module.exports = {
+        fooBar: 123
+    };
+
+}));
