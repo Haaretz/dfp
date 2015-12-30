0fc261648fe9b9579650d87f85c29fe7891f9aa9
KFlash <kflash123@gmail.com>
2015-12-31 06:54:29 +0800

    linting

--- test/browser/boily.js
+++ test/browser/boily.js
@@ -1,12 +1,12 @@
 import boily from '../../src';
 
-describe('Boily - unit tests for browser', () => {
+describe( 'Boily - unit tests for browser', () => {
 
-    it('should be a object', () => {
-        expect(boily).to.be.a.object;
-    })
+	it( 'should be a object', () => {
+		expect( boily ).to.be.a.object;
+	} );
 
-    it('should contain 123', () => {
-        expect(boily.foo).to.equal(123);
-    })
-});
+	it( 'should contain 123', () => {
+		expect( boily.foo ).to.equal( 123 );
+	} );
+} );
