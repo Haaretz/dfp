b8e1c7fb6364848e03bf1be9ddfdd411d6b085a2
KFlash <kflash123@gmail.com>
2015-12-31 13:30:58 +0800

    added mocha reporter settings

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -49,7 +49,16 @@ module.exports = function(config) {
 		// possible values: 'dots', 'progress'
 		// available reporters: https://npmjs.org/browse/keyword/karma-reporter
 		reporters: ['mocha', 'coverage'],
-
+		// reporter options
+		mochaReporter: {
+			colors: {
+				success: 'green',
+				info: 'bgYellow',
+				warning: 'cyan',
+				error: 'bgRed'
+			},
+			divider: ''
+		},
 		coverageReporter: {
 			reporters: [{
 				type: 'html',
