a22f4acf9413f015f61d07ae136a44cd04059dce
Tom Jacques <tejacques@gmail.com>
2015-12-31 02:32:40 -0500

    use node_modules instead of setup folder

--- config/webpack.dev.conf.js
+++ config/webpack.dev.conf.js
@@ -1,5 +1,5 @@
-const webpack = require('webpack')
-const glob = require('glob')
+const webpack = require('webpack');
+const glob = require('glob');
 
 const entries = glob.sync('./test/specs/**/*.spec.js')
 	.concat(glob.sync('./test/specs/**/*.spec.browser.js'))
@@ -7,27 +7,35 @@ const entries = glob.sync('./test/specs/**/*.spec.js')
 	.map(function(file) { return 'mocha!' + file; });
 
 module.exports = {
-	entry: entries,
-	output: {
-		path: './test',
-		filename: 'specs.js',
-		publicPath: 'http://localhost:8080/'
-	},
-	module: {
-		loaders: [{
-			test: /\.js$/,
-			exclude: /(dist|.git)/,
-			loader: 'babel-loader'
-		}]
-	},
-	devServer: {
-		contentBase: './test',
-		port: 8080,
-		noInfo: true,
-		hot: true,
-		inline: true
-	},
-	plugins: [
-		new webpack.HotModuleReplacementPlugin()
-	]
+  entry: entries,
+  output: {
+    path: './test',
+    filename: 'specs.js',
+    publicPath: 'http://localhost:8080/'
+  },
+  devtool: 'source-map',
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      exclude: /node_modules\/dist/,
+      loader: 'babel-loader'
+    }]
+  },
+  devServer: {
+    contentBase: './',
+    port: 8080,
+    noInfo: true,
+	hot: true,
+    inline: true,
+    proxy: {
+      '/': {
+        bypass: function(req, res, proxyOptions) {
+          return '/test/index.html';
+        }
+      }
+    },
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ]
 }
