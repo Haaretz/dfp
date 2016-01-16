8b4c4f4699a87f29bb6d08cc5d458b5c5a7ddc92
KFlash <kflash123@gmail.com>
2016-01-16 09:59:12 +0800

    version bump - v. 0.7.1

--- config/rollup.config.js
+++ config/rollup.config.js
@@ -1,12 +1,10 @@
 import * as p from 'path';
 import * as fs from 'fs';
-import zlib from 'zlib';
-import rollup from 'rollup';
+import {rollup} from 'rollup';
 import babel from 'rollup-plugin-babel';
 import npm from 'rollup-plugin-npm';
-import commonjs from 'rollup-plugin-commonjs';
 import replace from 'rollup-plugin-replace';
-import uglify from 'uglify-js';
+import uglify from 'rollup-plugin-uglify';
 import pack from '../package.json';
 
 const development = process.argv[2] === 'dev';
@@ -18,6 +16,9 @@ if ( development ) {
 	process.env.NODE_ENV = 'production'
 }
 
+/*
+ * Banner
+ **/
 const copyright =
 	'/*!\n' +
 	' * ' + pack.name + ' v' + pack.version + '\n' +
@@ -25,101 +26,50 @@ const copyright =
 	' * Released under the ' + pack.license + ' License.\n' +
 	' */'
 
-function createBundle() {
-	let bundle = rollup.rollup({
-		entry: p.resolve('src/index.js'),
-		plugins: [
-			babel({
-				babelrc: false,
-				presets: [
-					'es2015-rollup'
-				],
-				plugins: [
-					'transform-object-rest-spread',
-				]
-			}),
-			npm({
-				jsnext: true,
-				main: true,
-			}),
-			commonjs({
-				sourceMap: true
-			}),
-			replace({
-				'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
-				exclude: 'node_modules/**',
-				VERSION: pack.version,
-			})
+const entry = p.resolve('src/index.js');
+const dest  = p.resolve(`dist/boily.${production ? 'min.js' : 'js'}`);
+
+const bundleConfig = {
+	dest,
+	format: 'umd',
+	moduleName: 'Boily',
+	banner: copyright,
+	sourceMap: true,
+	globals: {
+		react: 'React',
+	},
+};
+
+let babelConfig = JSON.parse(fs.readFileSync('.babelrc', 'utf8'));
+babelConfig.babelrc = false;
+babelConfig.presets = babelConfig.presets.map((preset) => {
+	return preset === 'es2015' ? 'es2015-rollup' : preset;
+});
+
+let plugins = [
+	babel(babelConfig),
+	npm({
+		jsnext: true,
+		skip: [
+			'react',
 		],
-	});
+	}),
+	replace({
+		'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+		exclude: 'node_modules/**',
+		VERSION: pack.version,
+	}),
+];
 
-	// Cast as native Promise.
-	return Promise.resolve(bundle);
-}
-
-function zip() {
-	return new Promise(function(resolve, reject) {
-		fs.readFile('dist/' + pack.name + '.min.js', function(err, buf) {
-			if (err) return reject(err)
-			zlib.gzip(buf, function(err, buf) {
-				if (err) return reject(err)
-				fs.writeFile('dist/' + pack.name + '.min.js.gz', buf);
-			})
-		})
-	})
-}
-
-function writeBundle( bundle ) {
-	const filename = production ? pack.name + '.min.js' : pack.name + '.js';
-	const dest = p.resolve(`dist/${filename}`);
-
-	let result = bundle.generate({
-		format: 'umd',
-		moduleName: 'Boily',
-		banner: copyright,
-		sourceMap: true,
-		sourceMapFile: dest,
-		globals: {
-		},
-	});
-
-	if ( production ) {
-		result = uglify.minify(result.code, {
-			fromString: true,
-			inSourceMap: result.map,
-			outSourceMap: `${filename}.map`,
+if (production) {
+	plugins.push(
+		uglify({
 			warnings: false,
-		});
-
-		result.map = JSON.parse(result.map);
-	} else {
-		result.code += `\n//# sourceMappingURL=${filename}.map`;
-	}
-
-	let {
-		code,
-		map
-		} = result;
-
-	const throwIfError = (err) => {
-		if (err) {
-			throw err;
-		}
-
-		console.log('\x1b[1m\x1b[34m' + dest + '\x1b[39m\x1b[22m' + ' ' + (code.length / 1024).toFixed(2) + 'kb')
-	};
-
-	fs.writeFile(dest, code, throwIfError);
-	fs.writeFile(`${dest}.map`, JSON.stringify(map), throwIfError);
+		})
+	);
 }
 
-// -----------------------------------------------------------------------------
+let bundle = Promise.resolve(rollup({entry, plugins}));
+bundle.then(({write}) => write(bundleConfig));
 
-createBundle().then( ( bundle ) => {
-
-	writeBundle( bundle );
-
-	if ( production ) {
-		zip(); // gZip
-	}
-})
+process.on('unhandledRejection', (reason) => {throw reason;});
--- config/webpack.dev.conf.js
+++ config/webpack.dev.conf.js
@@ -7,35 +7,35 @@ const entries = glob.sync('./test/specs/**/*.spec.js')
 	.map(function(file) { return 'mocha!' + file; });
 
 module.exports = {
-  entry: entries,
-  output: {
-    path: './test',
-    filename: 'specs.js',
-    publicPath: 'http://localhost:8080/'
-  },
-  devtool: 'source-map',
-  module: {
-    loaders: [{
-      test: /\.js$/,
-      exclude: /node_modules\/dist/,
-      loader: 'babel-loader'
-    }]
-  },
-  devServer: {
-    contentBase: './',
-    port: 8080,
-    noInfo: true,
-	hot: true,
-    inline: true,
-    proxy: {
-      '/': {
-        bypass: function(req, res, proxyOptions) {
-          return '/test/index.html';
-        }
-      }
-    },
-  },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin()
-  ]
-}
+	entry: entries,
+	output: {
+		path: './test',
+		filename: 'specs.js',
+		publicPath: 'http://localhost:8080/'
+	},
+	devtool: 'source-map',
+	module: {
+		loaders: [{
+			test: /\.js$/,
+			exclude: /node_modules\/dist/,
+			loader: 'babel-loader'
+		}]
+	},
+	devServer: {
+		contentBase: './',
+		port: 8080,
+		noInfo: true,
+		hot: true,
+		inline: true,
+		proxy: {
+			'/': {
+				bypass: function(req, res, proxyOptions) {
+					return '/test/index.html';
+				}
+			}
+		}
+	},
+	plugins: [
+		new webpack.HotModuleReplacementPlugin()
+	]
+};
